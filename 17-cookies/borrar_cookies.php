930bd011fdbd96c7ce5246793f82e609e767c0d9
Arianny <guardandoLMPrimero2@gmail.com>
2020-07-23 11:12:04 +0200

    cookies

--- 17-cookies/borrar_cookies.php
+++ 17-cookies/borrar_cookies.php
@@ -1,8 +1,9 @@
 <?php
 
-if($_COOKIE['micookie']){
+if ($_COOKIE['micookie']) {
 	unset($_COOKIE['micookie']);
-	setcookie('micookie','',time()-100);
+	setcookie('micookie', '', time() - 100); //hay que caducar la cookiee
 }
 
+//redirecciones
 header('Location:ver_cookies.php');
